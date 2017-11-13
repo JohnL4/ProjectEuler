6cae5fdd338e8cc22954397afd385d7aa381aee5
John Lusk <john-github@how-hard-can-it-be.com>
2017-11-13 13:05:22 -0500

    Fix division type error.

--- Haskell/Problem004/app/Main.hs
+++ Haskell/Problem004/app/Main.hs
@@ -11,7 +11,7 @@ isPalindrome x =
   leftHalf == reverse rightHalf
   where
     n = length (show x)
-    halfLength = floor (n / 2)  -- TODO: use 'quot' operation, not (/)
+    halfLength = n `quot` 2  -- TODO: use 'quot' operation, not (/)
     leftHalf = take halfLength (show x)
     rightHalf = drop (halfLength + (if (n `mod` 2) == 1 then 1 else 0)) (show x)
                      
