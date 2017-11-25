e841c15956c79ce0d28372ea286640079c388b13
John Lusk <john-github@how-hard-can-it-be.com>
2017-11-24 19:22:55 -0500

    Make main function do the work; add more documentation from Maxima.

--- Haskell/Problem004/app/Main.hs
+++ Haskell/Problem004/app/Main.hs
@@ -5,18 +5,22 @@ import Text.Printf
 
 main :: IO ()
 main = do
-  printf "largest square palindrome: %d\n" (121::Integer)
+  printf "largest square palindrome: %d\n" largestSquarePalindrome
+  printf "largest palindrome:        %d\n" largestPalindrome
 
 -- Using Maxima (pgm), prove to yourself that x * (x+y) - (x-1) * (x+y+1) is always positive:
+-- 
 -- expand(x*(x+y)-(x-1)*(x+y+1)) --> y + 1
+-- 
 -- e.g., 995 * 998 - 994 * 999 = 4
+-- 
 -- So, the diagonal on the 2x2 matrix of products is the "ridge" (maxima), and the farther off the "ridge" you go, the
 -- farther from the "maximum" you fall.  Not sure how useful that is.
 
 -- If you have found some palindrome p and you are considering pairs of numbers (x1,x2) (where x1 >= x2) in constructing
 -- a larger palindrome, you don't need to consider x2's smaller than p/x1, since x1 * p/x1 = p.
 
-largestSquarePalindrome = maximum [ p^2 | p <- [999,998..1], isPalindrome (p^2) ] -- 698896, or 836^2
+largestSquarePalindrome = maximum [ p^2 | p <- ([999,998..1]::[Integer]), isPalindrome (p^2) ] -- 698896, or 836^2
 
 -- After above, define d3(n,m) := (m+836)*(n+836)−698896 (d3 was the third delta function I defined).
 --
@@ -28,8 +32,10 @@ largestSquarePalindrome = maximum [ p^2 | p <- [999,998..1], isPalindrome (p^2) 
 -- hyperbolic?).  So, I guess we should consider 700 to be our floor and cover all numbers above it.
 
 largestPalindrome = maximum $ [ (fst p) * (snd p)
-                              | p <- [ (x,y) | x <- [700..999], y <- [700..999] ], -- A list that is the Cartesian
-                                                                                   -- product of two other lists.
+                              -- A list that is the Cartesian product of two other lists.  Sort of.  It's really a
+                              -- "triangular" Cartesian product.  Also note that I had to specify the type of one of the
+                              -- constants ("[Integer]" means "list of Integers"), since Haskell can't guess them.
+                              | p <- [ (x,y) | x <- ([700..999]::[Integer]), y <- [x..999] ], 
                                 isPalindrome $ (fst p) * (snd p) ]
                                 
 
