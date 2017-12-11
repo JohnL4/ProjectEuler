4b1fbd4be9c5c0feac62fa9df53a7c53873ee8d2
John Lusk <john-github@how-hard-can-it-be.com>
2017-12-10 22:48:21 -0500

    Stupid brute-force approach doesn't work.

--- Haskell/Problem032/app/Main.hs
+++ Haskell/Problem032/app/Main.hs
@@ -6,11 +6,13 @@ import Lib
 
 main :: IO ()
 main = do
+
+  printf "head panDigitalTriples = %s\n" $ show $ head panDigitalTriples
   
   startTime <- getPOSIXTime
-
-  printf "sum of products in pandigital eualities: %d" sumPandigitalProducts
+  
+  printf "sum of products of all pandigital triples x * y = z: %d" sumPandigitalProducts
 
   stopTime <- getPOSIXTime
-  printf "\t(%s sec)\n" show $ stopTime - startTime
+  printf "\t(%s sec)\n" $ show (stopTime - startTime)
   
--- Haskell/Problem032/src/Lib.hs
+++ Haskell/Problem032/src/Lib.hs
@@ -1,17 +1,37 @@
 module Lib
-    ( sumPandigitalProducts
+    ( sumPandigitalProducts, panDigitalTriples
     ) where
 
-import Data.Map.Strict as Map
+import Data.Map.Strict as Map (empty, toList, insertWith)
 
-sumPandigitalProducts = 0
+maxSqrt :: Integer
+maxSqrt = ceiling $ sqrt 1e9
 
-noRepeatedDigits x = False
+sumPandigitalProducts =
+  sum $
+  Prelude.map (\(_,_,z) -> z) panDigitalTriples
 
--- | Frequency distribution of digits in single number x in form [(d,n)...], where d is digit in range [0..9] and n is
--- count of occurrence of digit.  TODO: be sure to eliminate 0s from consideration
-freqDist :: Integer -> [(Integer,Integer)]
-freqDist1 x =
+panDigitalTriples = [(x, y, x*y)
+                    | x <- [1..maxSqrt], noRepeatedDigitsOrZeros $ show x,
+                      y <- [100000000 `quot` x..1000000000 `quot` x], noRepeatedDigitsOrZeros $ show y,
+                      panDigital $ (show x) ++ (show y) ++ (show (x*y))
+                    ]
+
+-- | Return true iff x has no repeated digits AND no zeros.
+noRepeatedDigitsOrZeros s =
+  (foldr (&&) True $ map (1 ==) $ Prelude.map snd $ freqDist s)
+  &&
+  (not $ elem '0' $ map fst $ freqDist s)
+
+panDigital s =
+  noRepeatedDigitsOrZeros s
+  &&
+  length s == 9
+
+-- | Frequency distribution of digits in single number s in form [(d,n)...], where d is digit in range [0..9] and n is
+-- count of occurrence of digit.
+freqDist :: String -> [(Char,Integer)]
+freqDist s =
   Map.toList $
-  foldr (\ digit map -> Map.insertWith (1+) digit 1) Map.empty $
-  show x
+  Prelude.foldr (\ digit map -> Map.insertWith (+) digit 1 map) Map.empty $
+  s
